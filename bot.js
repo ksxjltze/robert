8da84d076569fb822cf815d6f1dd940ffd68a8d3
Lee Jia Keat <leejiakeat@gmail.com>
2021-07-30 21:40:50 +0800

    magic

--- bot.js
+++ bot.js
@@ -2,20 +2,22 @@ var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 
+require('dotenv').config();
+// const http = require('http');
+// port = 8080
 
-const http = require('http');
-port = 8080
+// const server = http.createServer((req, res) => {
+// // Set the response HTTP header with HTTP status and Content type
+// res.writeHead(200, {'Content-Type': 'text/plain'});
+// // Send the response body "Hello World"
+// res.end('Just for testing purposes\n');
+// });
 
-const server = http.createServer((req, res) => {
-// Set the response HTTP header with HTTP status and Content type
-res.writeHead(200, {'Content-Type': 'text/plain'});
-// Send the response body "Hello World"
-res.end('Just for testing purposes\n');
-});
+// console.log(process.env);
 
-server.listen(port, () => {
-console.log('Hello world listening on port', port);
-});
+// server.listen(port, () => {
+// console.log('Hello world listening on port', port);
+// });
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -26,7 +28,7 @@ logger.add(new logger.transports.Console, {
 logger.level = 'debug';
 // Initialize Discord Bot
 var bot = new Discord.Client({
-   token: auth.token,
+   token: process.env.token,
    autorun: true
 });
 
