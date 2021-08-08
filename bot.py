dc83aeea04feffb4a0b5d90873396cac4cb5d96c
Lee Jia Keat <leejiakeat@gmail.com>
2021-08-08 23:37:29 +0800

    test cogs

--- bot.py
+++ bot.py
@@ -31,6 +31,23 @@ the_time = datetime.now(tz=timezone)
 
 robert_guilds = {}
 
+class Progress(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+
+    #cog test
+    @commands.command()
+    async def hello(self, ctx, *, member: discord.Member = None):
+        """Says hello"""
+        member = member or ctx.author
+        if self._last_member is None or self._last_member.id != member.id:
+            await ctx.send('Hello {0.name}~'.format(member))
+        else:
+            await ctx.send('Hello {0.name}... This feels familiar.'.format(member))
+        self._last_member = member
+
+
 @tasks.loop(seconds = seconds_interval, minutes = minutes_interval, hours = hour_interval)
 async def hows_the_progress():
     for guild_id, guild in robert_guilds.items():
